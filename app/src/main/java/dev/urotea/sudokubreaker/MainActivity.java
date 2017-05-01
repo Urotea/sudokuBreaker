cc6c4b9479e584f9c5e37e474f57e70b1eb47695
Unknown <okakikakikaki@gmail.com>
2017-05-01 12:38:52 +0900

    dispaly textViews

--- app/src/main/java/dev/urotea/sudokubreaker/MainActivity.java
+++ app/src/main/java/dev/urotea/sudokubreaker/MainActivity.java
@@ -12,14 +12,25 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.GridLayout;
+import android.widget.TextView;
 
 import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.EActivity;
 import org.androidannotations.annotations.ViewById;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @EActivity(R.layout.activity_main)
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+    private final static int SUDOKU_COL = 9;
+    private final static int SUDOKU_ROW = 9;
+    private final static int SUDOKU_AREA = 9;
+
+    private List<TextView> sudokuList;
+    private List<GridLayout> sudokuAreaList;
 
     @ViewById(R.id.toolbar)
     Toolbar toolbar;
@@ -30,17 +41,70 @@ public class MainActivity extends AppCompatActivity
     @ViewById(R.id.nav_view)
     NavigationView navigationView;
 
+    @ViewById
+    GridLayout left_up;
+
+    @ViewById
+    GridLayout middle_up;
+
+    @ViewById
+    GridLayout right_up;
+
+    @ViewById
+    GridLayout left_middle;
+
+    @ViewById
+    GridLayout middle_middle;
+
+    @ViewById
+    GridLayout right_middle;
+
+    @ViewById
+    GridLayout left_bottom;
+
+    @ViewById
+    GridLayout middle_bottom;
+
+    @ViewById
+    GridLayout right_bottom;
+
     @AfterViews
     void init() {
-
         setSupportActionBar(toolbar);
-
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.setDrawerListener(toggle);
         toggle.syncState();
-
         navigationView.setNavigationItemSelectedListener(this);
+
+        sudokuList = new ArrayList<>();
+        for(int i=0;i<SUDOKU_COL * SUDOKU_ROW; i+=1) {
+            sudokuList.add(new TextView(this));
+        }
+        sudokuAreaList = new ArrayList<>();
+        sudokuAreaList.add(left_up);
+        sudokuAreaList.add(middle_up);
+        sudokuAreaList.add(right_up);
+        sudokuAreaList.add(left_middle);
+        sudokuAreaList.add(middle_middle);
+        sudokuAreaList.add(right_middle);
+        sudokuAreaList.add(left_bottom);
+        sudokuAreaList.add(middle_bottom);
+        sudokuAreaList.add(right_bottom);
+
+        int row = 0, col = 0, areaCount = 0;
+        for(TextView textView : sudokuList) {
+            textView.setText(row+","+col);
+            textView.setLayoutParams(new GridLayout.LayoutParams(
+                    GridLayout.spec(row), GridLayout.spec(col)
+            ));
+            sudokuAreaList.get(areaCount).addView(textView);
+            col += 1;
+            col %= 3;
+            row = col == 0 ? row + 1 : row;
+            row %= 3;
+            areaCount = (col == 0 && row == 0) ? areaCount + 1 : areaCount;
+        }
     }
 
     @Override
@@ -79,21 +143,6 @@ public class MainActivity extends AppCompatActivity
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
-        int id = item.getItemId();
-
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
-        }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
